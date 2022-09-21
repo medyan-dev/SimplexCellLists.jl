48d21344d825fade1bbfe534b49c91d8c3b10af7
nhz2 <nhz2@cornell.edu>
2022-09-21 12:33:21 -0400

    work

--- test/test_naive.jl
+++ test/test_naive.jl
@@ -2,7 +2,6 @@
 
 include("common.jl")
 
-T = SimplexCellLists.Naive
 
 @testset "constructor" begin
     empty_s = SimplexCellLists.Naive(0, 0, 0)
@@ -12,8 +11,6 @@ T = SimplexCellLists.Naive
     oneallgroup_s = SimplexCellLists.Naive(1, 1, 1)
     tenallgroup_s = SimplexCellLists.Naive(10, 10, 10)
 end
-
-
 @testset "setElements!" begin
     empty_s = SimplexCellLists.Naive(0, 0, 0)
     setElements!(empty_s, [], [], [])
@@ -38,8 +35,6 @@ end
         ],
     )
 end
-
-
 @testset "addElement!" begin
     onepointgroup_s = SimplexCellLists.Naive(1, 0, 0)
     setElements!(onepointgroup_s, [[[[1,2,3]],[[3,4,6]]]], [], [])
@@ -52,14 +47,21 @@ end
         [[[[1,2,3],[7,2,5]],[[3,4,6],[3,9,6]]]],
         [[[[1,2,3],[7,2,5],[7,9,5]],[[3,4,6],[3,9,6],[1,2,3]]]],
     )
+    @test 3 == addElement!(oneallgroup_s, 1, SA[SA_F32[3,4,6]])
+    @test 3 == addElement!(oneallgroup_s, 1, SA[SA_F32[3,4,6], SA_F32[4,4,6]])
+    @test 3 == addElement!(oneallgroup_s, 1, SA[SA_F32[3,4,6], SA_F32[4,4,6], SA_F32[5,5,6]])
 
     twoallgroup_s = SimplexCellLists.Naive(2, 2, 2)
     setElements!(twoallgroup_s, 
         [[[[1,2,3]],[[3,4,6]]],[]],
         [[],[[[1,2,3],[7,2,5]],[[3,4,6],[3,9,6]]]],
-        [
-            [[[1,2,3],[7,2,5],[7,9,5]],[[3,4,6],[3,9,6],[1,2,3]]],
-            [[[1,2,3],[7,2,5],[7,9,5]],[[3,4,6],[3,9,6],[1,2,3]]],
-        ],
+        [[],[]],
     )
+    @test 1 == addElement!(twoallgroup_s, 2, SA[SA_F32[3,4,6]])
+    @test 3 == addElement!(twoallgroup_s, 1, SA[SA_F32[3,4,6]])
+    @test 3 == addElement!(twoallgroup_s, 2, SA[SA_F32[3,4,6], SA_F32[4,4,6]])
+    @test 1 == addElement!(twoallgroup_s, 1, SA[SA_F32[3,4,6], SA_F32[4,4,6]])
+    @test 1 == addElement!(twoallgroup_s, 1, SA[SA_F32[3,4,6], SA_F32[4,4,6], SA_F32[5,5,6]])
+end
+@testset "mapSimplexElements" begin
 end
